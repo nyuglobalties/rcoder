e09ac6d01dda1d81dd6a7f95d69b96d1200669d8
Patrick Anker <patricksanker@gmail.com>
2020-01-27 15:30:21 -0500

    Fixed indentation in tests

--- tests/testthat/test-code.R
+++ tests/testthat/test-code.R
@@ -1,17 +1,17 @@
 context("Coding")
 
 expect_rc_error <- function(object, ...) {
-    expect_error(object, class = "rc_error", ...)
+  expect_error(object, class = "rc_error", ...)
 }
 
 test_that("code definitions are consistent", {
-    expect_rc_error(code(1, 1))
-    expect_rc_error(code(1, 1, description = 1))
-    expect_rc_error(code(NA, 1))
-    expect_rc_error(code(c("Bad", "Entry"), 1))
-    expect_rc_error(code("Oops", 1:10))
+  expect_rc_error(code(1, 1))
+  expect_rc_error(code(1, 1, description = 1))
+  expect_rc_error(code(NA, 1))
+  expect_rc_error(code(c("Bad", "Entry"), 1))
+  expect_rc_error(code("Oops", 1:10))
 
-    expect_identical(code("Yes", 1), code(value = 1, label = "Yes"))
+  expect_identical(code("Yes", 1), code(value = 1, label = "Yes"))
 })
 
 test_that("coding creation makes sense", {
@@ -22,7 +22,7 @@ test_that("coding creation makes sense", {
 })
 
 test_that("Safe coding evaluation works", {
-    expr <- bquote(coding(code("Yes", 1), code("No", 2)))
+  expr <- bquote(coding(code("Yes", 1), code("No", 2)))
 
-    expect_identical(eval_coding(expr), coding(code("Yes", 1), code("No", 2)))
+  expect_identical(eval_coding(expr), coding(code("Yes", 1), code("No", 2)))
 })
